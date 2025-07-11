e292e7d2a88920f6b00a847ba2d15fd12e2b0138
mfeti <mfeti2175@gmail.com>
2025-07-11 08:28:33 +0300

    feat: add test for fizz buzz

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -6,3 +6,18 @@ describe("max", () => {
     expect(max(1, 2)).toBe(2);
   });
 });
+
+describe("fizzBuzz", () => {
+  it("should return 'Fizz' for multiples of 3", () => {
+    expect(fizzBuzz(3)).toBe("Fizz");
+  });
+  it("should return 'Buzz' for multiples of 5", () => {
+    expect(fizzBuzz(5)).toBe("Buzz");
+  });
+  it("should return 'FizzBuzz' for multiples of both 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+  });
+  it("should return the number as a string for other numbers", () => {
+    expect(fizzBuzz(7)).toBe("7");
+  });
+});
